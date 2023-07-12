a20af7675dfddda382afc01b462fbbf6fbeec26e
Chris Cheng <yifan.cheng.1998@gmail.com>
2023-07-12 19:31:56 +1000

    add func to generate ssl certificate

--- ssl_extract.cpp
+++ ssl_extract.cpp
@@ -1,1 +1,45 @@
+#define OUTPUT_CERTIFICATE_PATH "certificates/"  // if no such folder, there will be error, such change it to empty string
 
+#include <iostream>
+#include <string>
+#include <pcap.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
+#include <openssl/x509.h>
+
+using namespace std;
+
+struct SSLPacketHeader {
+    uint8_t type;
+    uint16_t version;
+    uint16_t length;
+};
+
+void generate_certificate(X509* cert) {
+  // cout << "reach cert printing" << endl;
+  X509_NAME* subjectName = X509_get_subject_name(cert);
+  int commonNameIndex = X509_NAME_get_index_by_NID(subjectName, NID_commonName, -1);
+  if (commonNameIndex != -1) {
+    // Get the Common Name (CN) field value as an ASN1_STRING
+    ASN1_STRING* commonNameASN1 = X509_NAME_ENTRY_get_data(X509_NAME_get_entry(subjectName, commonNameIndex));
+
+    // Convert the ASN1_STRING to a C string
+    string commonName = reinterpret_cast<const char*> (ASN1_STRING_get0_data(commonNameASN1));
+    string fn = OUTPUT_CERTIFICATE_PATH + commonName + ".pem";
+    const char* filename = fn.c_str();
+    try {
+      FILE* file = fopen(filename, "w");
+      PEM_write_X509(file, cert);
+      fclose(file);
+      cout << "Successfully generated pem certificate file. File name: " << filename << endl;
+    } catch (const exception& ex) {
+      cerr << "Error writing pem file. Make sure the folder 'certificates' exists. Error: " << ex.what() << endl;
+    }
+  } else {
+    char* subjectNameString = X509_NAME_oneline(subjectName, nullptr, 0);
+    cout << "Common Name not found in the subject name. subject name: " << subjectNameString << endl;
+  }
+  // X509_print_fp(stdout, cert); // Print certificate details to stdout
+}
